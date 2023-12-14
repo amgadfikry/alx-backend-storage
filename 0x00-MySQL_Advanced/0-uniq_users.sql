19a095c531e7e3e53c5fbe120548c1fcdd7da77c
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-14 14:45:53 +0200

    create sql script that create table with specific requirements

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+--  creates a table users with specific requirements
+--  create table with columns id, email, name
+CREATE TABLE IF NOT EXISTS users(
+  id INT NOT NULL AUTO_INCREMENT,
+  email VARCHAR(255) NOT NULL,
+  name VARCHAR(255),
+  PRIMARY KEY (id),
+  UNIQUE (email)
+);
