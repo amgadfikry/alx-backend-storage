4f49740ef4fca457348849b833524a58d9c4ddc4
amgadfikry <dr.amgad_sh92@yahoo.com>
2023-12-19 10:45:22 -0800

    dding the top 10 of the most present IPs in the collection nginx of the database logs

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Python script that provides some stats about Nginx logs """
+from pymongo import MongoClient
+
+
+def main():
+    """ main function run if excute"""
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    collection = client.logs.nginx
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+
+    total = collection.count_documents({})
+    methods_total = [
+            collection.count_documents({'method': m}) for m in methods]
+    status = collection.count_documents({'method': 'GET', 'path': '/status'})
+    print('{} logs'.format(total))
+    print('Methods:')
+    for x in range(len(methods)):
+        print('\tmethod {}: {}'.format(methods[x], methods_total[x]))
+    print('{} status check'.format(status))
+    ips = collection.aggregate([
+        {'$group': {
+            '_id': '$ip',
+            'count': {'$sum': 1}}},
+        {'$sort': {'count': -1}},
+        {'$limit': 10}
+        ])
+    print('IPs:')
+    for x in ips:
+        print('\t{}: {}'.format(x['_id'], x['count']))
+
+
+if __name__ == "__main__":
+    main()
